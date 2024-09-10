5a8c13d2167325c9aec8d9c4b823863af12568d0
Simas <simonas.aruna@gmail.com>
2024-09-10 14:35:09 +0300

    added regex util

---
+++ src/main/java/org/sa/util/RegExUtil.java
@@ -0,0 +1,36 @@
+package org.sa.util;
+
+public class RegExUtil {
+  public static final String LETTER = "[a-zA-Z]";
+  public static final String NON_LETTER = "[^a-zA-Z]";
+  public static final String ALPHANUMERIC = "\\w";  // [a-zA-Z0-9_]
+  public static final String NON_ALPHANUMERIC = "\\W";  // [^a-zA-Z0-9_]
+
+
+  public static final String OR = "|";
+  public static final String LETTER_WORD_EDGE = getLetterWordEdge();
+  public static final String ALPHANUMERIC_UNDERSCORE_WORD_EDGE = "\\b";
+
+
+  public static String[] splitOnLetterWordEdges(String s) {
+    return s.split(LETTER_WORD_EDGE);
+  }
+
+  private static String lookBehind(String element) {
+    String lookbehindStart = "(?<=";
+    String lookbehindEnd = ")";
+    return lookbehindStart + element + lookbehindEnd;
+  }
+
+  private static String lookAhead(String element) {
+    String lookaheadStart = "(?=";
+    String lookaheadEnd = ")";
+    return lookaheadStart + element + lookaheadEnd;
+  }
+
+  private static String getLetterWordEdge() {
+    String edgeWordToNonword = lookBehind(LETTER) + lookAhead(NON_LETTER);
+    String edgeNonwordToWord = lookBehind(NON_LETTER) + lookAhead(LETTER);
+    return edgeNonwordToWord + OR + edgeWordToNonword;
+  }
+}
